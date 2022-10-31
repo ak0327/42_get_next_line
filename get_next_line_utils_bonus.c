1ed8c5230c9deccc4cb1a55348874a7aa0574066
Akira TANIDA <akira@MPB16.local>
2022-10-31 19:31:31 +0900

    Rename *_utils_bouns.c to *_utils_bonus.c

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bouns.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 19:09:28 by takira            #+#    #+#             */
+/*   Updated: 2022/10/31 19:22:42 by takira           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = s;
+	while (i < n)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (dst == NULL && src == NULL)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (count == 0 || size == 0)
+		return (ft_calloc(1, 1));
+	if (count > SIZE_MAX / size)
+		return (NULL);
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memset(ptr, 0, count * size);
+	return (ptr);
+}
+
+char	*ft_strjoin(char *dst, char *src, size_t size)
+{
+	char			*ret;
+	const size_t	dstsize = ft_strlen(dst);
+
+	ret = (char *)ft_calloc(sizeof(char), dstsize + size + 1);
+	if (!ret)
+	{
+		free(dst);
+		return (NULL);
+	}
+	ft_memcpy(ret, dst, dstsize);
+	ft_memcpy(&ret[dstsize], src, size);
+	free(dst);
+	return (ret);
+}
